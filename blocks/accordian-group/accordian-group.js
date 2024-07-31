2f51e594f44f186b1ed1f7f08060108cf4566942
Kaushik Khara <Kaushik.Khara@teknopoint.local>
2024-07-31 18:34:25 +0530

    accordin group

---
+++ blocks/accordian-group/accordian-group.js
@@ -0,0 +1,19 @@
+import { generateAccordionDOM } from "../accordion/accordion.js";
+export default async function decorate(block) {
+  const resp = await fetchPlaceholders();
+  //console.log("placeholder resp :: ", resp);
+  // each row is an accordion entry
+  const accordions = [...block.children];
+  // loop through all accordion blocks
+  [...accordions].forEach((accordion) => {
+    // generate the accordion
+    const accordionDOM = generateAccordionDOM(accordion);
+    // empty the content ,keep root element with UE instrumentation
+    accordion.textContent = "";
+    // add block classes
+    accordion.classList.add("accordion", "block");
+    accordion.append(accordionDOM);
+  });
+  // use same styling as shade-box from /docs
+  block.classList.add("shade-box");
+}
