3b180c140bfabb804666e941737cff712c1d58db
Kaushik Khara <Kaushik.Khara@teknopoint.local>
2024-07-31 18:42:36 +0530

    accordin group

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,8 +1,8 @@
-/*
+/* /*
  * Accordion Block
  * Recreate an accordion
  * https://www.hlx.live/developer/block-collection/accordion
- */
+ *)
 
 function hasWrapper(el) {
   return !!el.firstElementChild && window.getComputedStyle(el.firstElementChild).display === 'block';
@@ -30,4 +30,41 @@ export default function decorate(block) {
     details.append(summary, body);
     row.replaceWith(details);
   });
+}
+ */
+
+export function generateAccordionDOM(block) {
+  const details = document.createElement("details");
+  const summary = document.createElement("summary");
+  details.append(summary);
+  Array.from(block.children).forEach(async (element, i) => {
+    if (i === 0) {
+      // const heading = element.querySelector("h2,h3,h4,h5,h6");
+      const heading = element;
+      summary.append(heading || element.textContent.trim());
+    } else {
+      try {
+        const url = element.innerText.trim();
+        const isurl = url.includes(".json");
+        const elementText = element.innerHTML.replace(/&lt;/g, "<").replace(/&gt;/g, ">")
+        var elementDiv = document.createElement("div");
+        if (isurl) {
+          let data = await renderDataFromAPI(url);
+          const documentHTMl = KYCDocuments(data);
+          elementDiv.innerHTML = documentHTMl;
+        } else {
+          elementDiv.innerHTML = elementText;
+        }
+      } catch (error) {
+        console.warn(error);
+      }
+      details.append(elementDiv);
+    }
+  });
+  return details;
+}
+export default function decorate(block) {
+  const dom = generateAccordionDOM(block);
+  block.textContent = "";
+  block.append(dom);
 }
