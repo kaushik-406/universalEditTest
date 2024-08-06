cd74263baa7adc3d2694c11623931e10936741fd
Kaushik Khara <Kaushik.Khara@teknopoint.local>
2024-08-06 15:45:57 +0530

    new components

--- blocks/listitem/listitem.js
+++ blocks/listitem/listitem.js
@@ -1,9 +1,9 @@
 export default function decorate(block) {
     const [liWrapper] = block.children;
-    // console.log(liWrapper);
+    console.log(liWrapper);
   
     const liblock = document.createElement('li');
-    // liblock.textContent = liWrapper.textContent.trim();
+    liblock.textContent = liWrapper.textContent.trim();
     liWrapper.replaceChildren(liblock);
   }
   
