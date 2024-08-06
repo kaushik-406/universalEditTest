f5fbd6a049453e5628d76771d98eea6cc86420d8
Kaushik Khara <Kaushik.Khara@teknopoint.local>
2024-08-06 19:29:38 +0530

    new components

---
+++ blocks/unorderedlist/unorderedlist.js
@@ -0,0 +1,12 @@
+export default function decorate(block) {
+    const props = Array.from(block.children, (row) => row.firstElementChild);
+    // const [image, text] = props;
+  
+    const ulblock = document.createElement('ul');
+    ulblock.appendChild(liblock);
+    // console.log(ulblock);
+  
+    block.innerHTML = '';
+    block.appendChild(ulblock);
+  }
+  
